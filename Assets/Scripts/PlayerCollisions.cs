7b1dbaf341499c2eb8fcf02e6b9260b0e92a09b8
Oskar Carlbaum <oskarcarlbaum@gmail.com>
2018-06-09 16:03:09 +0200

    Add base of player collision script based on tags

---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+
+  void OnCollisionEnter(Collision collision)
+  {
+    switch (collision.gameObject.tag.ToString())
+    {
+      case "Player":
+        Debug.Log("Player collided with another player");
+        break;
+      case "Blackhole":
+        Debug.Log("Player collided with THE BLACK HOLE AAAAAAAH!");
+        break;
+      default:
+        break;
+    }
+  }
+}
