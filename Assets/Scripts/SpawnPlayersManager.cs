a9f38a870c46bedef2a28a043fecfb2d2a43dedc
Michael Novén <michael.f.noven@gmail.com>
2018-06-09 17:56:50 +0200

    Spawn players with different colors

--- Assets/Scripts/SpawnPlayersManager.cs
+++ Assets/Scripts/SpawnPlayersManager.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPlayersManager : MonoBehaviour {
+	private static readonly Color[] PLAYER_COLORS = { Color.red, Color.green, Color.blue, Color.yellow };
+
 	// Use this for initialization
 	void Start () {
 		PhysicsObjectController physicsObjectController = GetComponent<PhysicsObjectController>();
 		for (int i = 0; i < GlobalData.numPlayers; ++i) {
 			GameObject playerGameObject = (GameObject)Resources.Load("Prefabs/Player", typeof(GameObject));
+			
+			// Set unique color and name
 			playerGameObject.name = "Player " + (i + 1);
+
+			Debug.Log(i);
+			Debug.Log(PLAYER_COLORS[i]);
+			
 			PlayerMovement playerMovementComponent = playerGameObject.GetComponent<PlayerMovement>();
 			playerMovementComponent.playerType = (i + 1);
 			physicsObjectController.PhysicsObjects.Add(playerGameObject);
-			Instantiate(playerGameObject, new Vector3((i + 1) * 5, 0, 0), Quaternion.identity);
+
+			GameObject go = Instantiate(playerGameObject, new Vector3((i + 1) * 5, 0, 0), Quaternion.identity);	
+			go.GetComponent<MeshRenderer>().material.SetColor("_Color", PLAYER_COLORS[i]);
 		}
 	}
 	
