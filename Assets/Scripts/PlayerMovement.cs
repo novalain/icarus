b72d7504a037012b5ead0bfa91c9774a046fe21b
Michael Novén <michael.f.noven@gmail.com>
2018-06-09 14:46:32 +0200

    Replace player movement script in prefab

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+	private Rigidbody _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		_rigidBody.AddForce (movement * speed);
+	}
+}
