2a5f16ec5a82ce9bf8ed580449a6ea9328b12094
jarnald <johan_darnald@hotmail.com>
2018-06-09 17:33:40 +0200

    Added buttons for changing number of player
    Need to implement the logic

---
+++ Assets/Scripts/NumberOfPlayersPicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberOfPlayersPicker : MonoBehaviour {
+    public int numberOfPlayers = 2;
+    public Button morePlayersButton;
+    public Button lessPlayersButton;
+    public Text numberOfPlayersText;
+	// Use this for initialization
+	void Start () {
+        numberOfPlayersText.text = "Players: " + numberOfPlayers;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RemovePlayer()
+    {
+        if (numberOfPlayers > 2) numberOfPlayers -= 1;
+        numberOfPlayersText.text = "Players: " + numberOfPlayers;
+    }
+
+    public void AddPlayer()
+    {
+        if (numberOfPlayers < 4) numberOfPlayers += 1;
+        numberOfPlayersText.text = "Players: " + numberOfPlayers;
+    }
+}
