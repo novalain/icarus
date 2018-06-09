c98820e1d9d971d88b6cb36a471efaa120ee48be
jarnald <johan_darnald@hotmail.com>
2018-06-09 14:11:44 +0200

    fixed gravity
    fixed gravity

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 [RequireComponent(typeof(PhysicsData))]
 public class Physics : MonoBehaviour {
     public GameObject controller;
-    private Vector3 velocity;
+    public Vector3 initialVelocity;
 
-    private PhysicsObjectController  poc;
+
+    private PhysicsObjectController  _physicsObjectController;
+    private Rigidbody _rigidbody;
+    private PhysicsData _physicsData;
 	// Use this for initialization
 	void Start () {
-        velocity = new Vector3(0, 0, 0);
-        poc = controller.GetComponent<PhysicsObjectController>();
+        _physicsData = GetComponent<PhysicsData>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _physicsObjectController = controller.GetComponent<PhysicsObjectController>();
+        _rigidbody.velocity = initialVelocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		foreach(GameObject planet in poc.PhysicsObjects)
+		foreach(GameObject planet in _physicsObjectController.PhysicsObjects)
         {
+            if (GameObject.ReferenceEquals(gameObject, planet)) continue;
+            PhysicsData otherPhysicsData = planet.GetComponent<PhysicsData>();
 
+            float force = _physicsData.mass * otherPhysicsData.mass / Mathf.Pow(Vector3.Distance(transform.position, planet.transform.position), 2);
+            Vector3 forceVec =  (planet.transform.position - transform.position).normalized * force;
+            _rigidbody.AddForce(forceVec);
         }
 	}
 
