df03067b1b181f2528f8e3d93ef471dd0a0df89c
Michael Novén <michael.f.noven@gmail.com>
2018-06-10 13:52:11 +0200

    New main menu

---
+++ Assets/Scripts/SpinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(Vector3.right * Time.deltaTime * 10f);
+        // ...also rotate around the World's Y axis
+        transform.Rotate(Vector3.up * Time.deltaTime * 10f, Space.World);
+	}
+}
