3ad544fadd53d463ed5a87028d44af5b841262ae
Oskar Carlbaum <oskarcarlbaum@gmail.com>
2018-06-10 11:45:00 +0200

    Kill asteroids if distance from origo is too large

---
+++ Assets/Scripts/KillDistance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillDistance : MonoBehaviour {
+
+  // Use this for initialization
+  public float killDistanceFromOrigo = 100;
+
+  void removeGameObjectFromScene(GameObject gObject)
+  {
+    GameObject physicsObjectController = GameObject.Find("GameController");
+    physicsObjectController.GetComponent<PhysicsObjectController>().PhysicsObjects.Remove(gObject);
+    Destroy(gObject);
+  }
+
+
+  // Update is called once per frame
+  void Update() {
+    if (Vector3.Distance(gameObject.transform.position, new Vector3(0, 0, 0)) > killDistanceFromOrigo)
+    {
+      Debug.Log("Deleting object");
+      removeGameObjectFromScene(gameObject); 
+    }
+  }
+}
